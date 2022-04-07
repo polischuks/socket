9610cdd62962c23ab14ff6fc4d80fcdb552c84de
polischuks <polischuk0107@gmail.com>
2022-04-07 19:49:24 +0300

    Modify message for onClosing method

--- src/main/kotlin/client/EchoWebSocketListener.kt
+++ src/main/kotlin/client/EchoWebSocketListener.kt
@@ -4,11 +4,13 @@ import okhttp3.Response
 import okhttp3.WebSocket
 import okhttp3.WebSocketListener
 import okio.ByteString
+import kotlin.system.exitProcess
 
 class EchoWebSocketListener : WebSocketListener() {
 
     override fun onOpen(webSocket: WebSocket, response: Response) {
         super.onOpen(webSocket, response)
+        println("Client started")
     }
 
     override fun onMessage(webSocket: WebSocket, text: String) {
@@ -22,14 +24,18 @@ class EchoWebSocketListener : WebSocketListener() {
 
     override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
         super.onClosing(webSocket, code, reason)
-        println("You input command Exit. Bye")
+        println("You input command Exit.")
     }
 
     override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
         super.onClosed(webSocket, code, reason)
+        println("Program closed. Bye!")
     }
 
     override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
         super.onFailure(webSocket, t, response)
+        println("Error: server not found.\n" +
+                "Program closed.")
+        exitProcess(0)
     }
 }
--- src/main/kotlin/client/Main.kt
+++ src/main/kotlin/client/Main.kt
@@ -7,18 +7,19 @@ import java.util.concurrent.TimeUnit
 import kotlin.system.exitProcess
 
 fun main() {
+    println("Connecting...")
     val websockets = connect()
-    println("Client started")
-
+    Thread.sleep(2000)
     while (true) {
         println("Input message or command Exit ")
         when (val msg = readLine()!!) {
-            "exit" -> {
+            "Exit" -> {
                 websockets.close(1000,"Exit")
                 exitProcess(0)
             }
             else -> {
                 websockets.send(msg)
+                Thread.sleep(1500)
                 continue
             }
         }
@@ -27,7 +28,7 @@ fun main() {
 
 fun connect(): WebSocket {
     val client = OkHttpClient.Builder()
-        .readTimeout(3, TimeUnit.SECONDS)
+        .readTimeout(1, TimeUnit.SECONDS)
         .build()
     val wsURN = "ws://127.0.0.1:8885"
     val request = Request.Builder()
